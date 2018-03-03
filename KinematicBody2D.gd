fe6b88716be85ccc1f280b9b4ee26ab8682d3c84
PhillipNewberry <phillip.newberry@indecks.co>
2018-03-03 11:16:49 +0000

    New game character file made. Now prints Hello World when started.

---
+++ KinematicBody2D.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	print("Hello world")
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
