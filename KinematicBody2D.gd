1fdb79196d4d6648085894f67fcebbf685404428
PhillipNewberry <phillip.newberry@indecks.co>
2018-03-03 18:45:59 +0000

    Coffee can now be drank!

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,12 +1,18 @@
 extends KinematicBody2D
 
+onready var handsArea = $CoffeeStealer
+onready var hands = $CoffeeStealer/InteractionTrigger
+
 #Variables declared to make the game work... 
 #It just works... (tm)
 var movementVariable = Vector2(0,0)
 
+var coffeesCollected = 0
+
 func _ready(): #As soon as the object is put into the game... run this:
 	set_physics_process(true) #Look out for _physics_process(delta)
 	print("This is the brain - reading loud and clear..!")
+	handsArea.connect("area_entered", self, "_on_area_entered") #prepares to take coffees
 
 func _physics_process(delta): #Gets checked every physics check 
 	#Movement via keys 
@@ -21,10 +27,12 @@ func _physics_process(delta): #Gets checked every physics check
 		movementVariable.y+=1
 	movementVariable.normalized()
 	move_and_slide(movementVariable*350)
-	
-	#Checking if we have touched coffee!
-	# get_node("InteractionTrigger"):
-	#	pass
+
+func _on_area_entered(area):
+	if area.name == "CoffeeToTakeArea":
+		coffeesCollected = coffeesCollected +1 
+		print(coffeesCollected)
+
 		
 #If (our character) touches (someone (who has a coffee))
 	#Remove the coffee from the person
--- scn/coffee.gd
+++ scn/coffee.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-onready var coffee_area = $Coffee/Area2D #get_node('Coffee/Area2D')
+onready var coffee_area = $Coffee/CoffeeToTakeArea #get_node('Coffee/Area2D')
 onready var coffee = $Coffee
 
 func _ready():
@@ -8,4 +8,4 @@ func _ready():
 	
 func _on_area_entered(area):
 	if area.name == 'CoffeeStealer':
-		coffee.visible = false
+		coffee.queue_free()
